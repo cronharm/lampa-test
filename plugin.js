8778f74f4bacc7dd1318585f5746f7685fce2137
cronharm <95356195+cronharm@users.noreply.github.com>
2025-11-28 00:12:36 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,36 +1,37 @@
 (function () {
     'use strict';
 
-    // Чтобы не добавлять кнопку сто раз
-    let buttonAdded = false;
-
     function addButton(e) {
-      if (e.render.find('.lampac--buttonv').length) return;
-      var btn = $(Lampa.Lang.translate(button));
-      btn.on('hover:enter', function() {
-        resetTemplates();
-        Lampa.Component.add('lampavod', component);
-        Lampa.Activity.push({
-          url: '',
-          title: 'test btn',
-          component: 'lampavod',
-          search: e.movie.title,
-          search_one: e.movie.title,
-          search_two: e.movie.original_title,
-          movie: e.movie,
-          page: 1
+        if (e.render.find('.lampac--buttonv').length) return;
+
+        let btn = $('<div class="lampac--buttonv selector">Смотреть онлайн</div>');
+
+        btn.on('hover:enter', function () {
+            resetTemplates();
+            Lampa.Component.add('lampavod', component);
+            Lampa.Activity.push({
+                url: '',
+                title: 'test btn',
+                component: 'lampavod',
+                search: e.movie.title,
+                search_one: e.movie.title,
+                search_two: e.movie.original_title,
+                movie: e.movie,
+                page: 1
+            });
         });
-      });
-      e.render.before(btn);
+
+        e.render.after(btn); 
     }
-    Lampa.Listener.follow('full', function(e) {
-      if (e.type == 'complite') {
-        addButton({
-          render: e.object.activity.render().find('.view--torrent'),
-          movie: e.data.movie
-        });
-      }
+
+    Lampa.Listener.follow('full', function (e) {
+        if (e.type === 'complite') {
+            addButton({
+                render: e.object.html.find('.view--torrent'),
+                movie: e.data.movie
+            });
+        }
     });
 
-    console.log('Плагин «Смотреть онлайн» загружен (без ошибок)');
+    console.log('Плагин «Смотреть онлайн» загружен');
 })();
